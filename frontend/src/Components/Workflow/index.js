e1d2a09d70a02a4c674713bc703cde5ae9f53099
Joachim Baumann <joachimbaumann1@gmail.com>
2021-06-22 00:39:47 +0200

    added navbar menu

---
+++ frontend/src/Components/Workflow/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Workflow.css';
+import Header from '../Header';
+
+export function Workflow() {
+  return (
+    <div className="Worflow">
+      <Header title="Complete all steps..."/>
+    </div>
+  );
+}
+
+export default Workflow;
