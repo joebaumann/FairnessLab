7af6002ca68597c3d0e0c0ef857e4647651817a9
Joachim Baumann <joachimbaumann1@gmail.com>
2021-06-22 22:39:15 +0200

    implemented  dataset selection for workflow step1 content

--- frontend/src/Components/Workflow/index.js
+++ frontend/src/Components/Workflow/index.js
@@ -1,7 +1,6 @@
-import React from "react";
+import React, { useState } from 'react';
 import './Workflow.css';
 import Header from '../Header';
-import Step1 from '../WorkflowSteps/Step1';
 
 import PropTypes from 'prop-types';
 import { makeStyles, withStyles } from '@material-ui/core/styles';
@@ -18,8 +17,17 @@ import CheckCircleTwoToneIcon from '@material-ui/icons/CheckCircleTwoTone';
 import StepConnector from '@material-ui/core/StepConnector';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 
-
+// creating the forceUpdate hook
+function useForceUpdate(){
+  const [value, setValue] = useState(0); // integer state
+  return () => setValue(value => value + 1); // update the state to force render
+}
 
 const ColorlibConnector = withStyles({
   alternativeLabel: {
@@ -121,18 +129,63 @@ const useStyles = makeStyles((theme) => ({
   instructions: {
     marginTop: theme.spacing(1),
     marginBottom: theme.spacing(1)
-  }
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
 }));
 
 function getSteps() {
   return ['Choose dataset', 'Name labels', 'Utility function', 'Visualizations'];
 }
 
+function Step1(props) {
 
-function getStepContent(step) {
-  switch (step) {
+  const classes = useStyles();
+
+  const handleChange = (event) => {
+    // update state: currently selected dataset
+    props.handleChangeDataset(event.target.value);
+  };
+
+  return (
+    <div className="Step1">
+
+      'Please select or add a dataset:'
+      <br/>
+      Selected: {props.dataset}
+      <br/>
+
+      <FormControl variant="outlined" className={classes.formControl}>
+        <InputLabel id="demo-simple-select-outlined-label">Dataset</InputLabel>
+        <Select
+          labelId="demo-simple-select-outlined-label"
+          id="demo-simple-select-outlined"
+          value={props.dataset}
+          onChange={handleChange}
+          label="Dataset"
+          >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          {/* TODO: use available datasets as selection items */}
+          <MenuItem value={"dataset1"}>dataset1</MenuItem>
+          <MenuItem value={"dataset2"}>dataset2</MenuItem>
+          <MenuItem value={"dataset3"}>dataset3</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+  )
+}
+
+function getStepContent(activeStep, dataset, handleChangeDataset) {
+  switch (activeStep) {
     case 0:
-      return 'Please select or add a dataset:';
+      return <Step1 dataset={dataset} handleChangeDataset={handleChangeDataset} />;
     case 1:
       return 'Please define the names of the following labels:';
     case 2:
@@ -146,9 +199,13 @@ function getStepContent(step) {
 
 export function Workflow() {
   const classes = useStyles();
-  const [activeStep, setActiveStep] = React.useState(0);
-  const [completed, setCompleted] = React.useState({});
+  const [activeStep, setActiveStep] = useState(0);
+  const [completed, setCompleted] = useState({});
   const steps = getSteps();
+  
+  const [dataset, setDataset] = useState(''); // step1 state
+  
+  const forceUpdate = useForceUpdate();
 
   const totalSteps = () => {
     return steps.length;
@@ -188,12 +245,39 @@ export function Workflow() {
     const newCompleted = completed;
     newCompleted[activeStep] = true;
     setCompleted(newCompleted);
-    handleNext();
+    // handleNext(); // continue to the next step automatically as soon as the active step is completed
+    forceUpdate(); // force the stepper to rerender after step has been marked as complete
+  };
+
+  const handleUncomplete = () => {
+    const newCompleted = completed;
+    newCompleted[activeStep] = false;
+    setCompleted(newCompleted);
+    // handleNext(); // continue to the next step automatically as soon as the active step is uncompleted
+    forceUpdate(); // force the stepper to rerender after step has been marked as uncomplete
   };
 
   const handleReset = () => {
     setActiveStep(0);
     setCompleted({});
+  };
+
+
+  const handleChangeDataset = (selectedDataset) => {
+    // update state: currently selected dataset
+    setDataset(selectedDataset);
+
+    // update state: is the current step completed?
+    console.log(`Selected dataset: ${selectedDataset}!`);
+    if (selectedDataset === "") {
+      // mark current step as completed
+      handleUncomplete();
+    }
+    else {
+      // mark current step as uncompleted
+      handleComplete();
+
+    }
   };
 
   return (
@@ -228,7 +312,7 @@ export function Workflow() {
           ) : (
             <div>
               <Typography className={classes.instructions}>
-                {getStepContent(activeStep)}
+                {getStepContent(activeStep, dataset, handleChangeDataset)}
               </Typography>
               <div>
                 <Button
@@ -250,6 +334,13 @@ export function Workflow() {
                   (completed[activeStep] ? (
                     <Typography variant="caption" className={classes.completed}>
                       Step {activeStep + 1} already completed
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={handleUncomplete}
+                      >
+                        Uncomplete Step Manually
+                      </Button>
                     </Typography>
                   ) : (
                     <Button
@@ -259,7 +350,7 @@ export function Workflow() {
                     >
                       {completedSteps() === totalSteps() - 1
                         ? "Finish"
-                        : "Complete Step"}
+                        : "Complete Step Manually"}
                     </Button>
                   ))}
               </div>
