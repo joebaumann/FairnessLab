6ea14a86caddd437186886473e7012bc9d857c3d
hcorinna <12029382+hcorinna@users.noreply.github.com>
2021-10-18 22:01:21 +0200

    Update About.js

--- frontend/src/Components/OtherPages/About.js
+++ frontend/src/Components/OtherPages/About.js
@@ -5,8 +5,14 @@ function About() {
   return (
     <div>
       <Header title="About"/>
-      This tool has been built by a team of researchers at the Zurich University of Applied Sciences, the University of Zurich and the University of St. Gallen.
-      Learn more about us here: <a href="https://fair-ai.ch" target="_blank">https://fair-ai.ch</a>
+      Hallo zusammen!<br/>
+      Vielen Dank, dass ihr euch bereit erklärt habt bei unserem Experiment mitzumachen.
+      Auf dem von uns ausgeteilten Informationsblatt findet ihr genaue Informationen zu diesem Experiment.
+      Dieses Tool selbst ist bisher noch sehr einfach aufgebaut. Navigiert einfach zum Punkt "Improve" im Menü oben, um loszulegen.
+      <br/>
+      Danke nochmal, wir schätzen eure Teilnahme sehr!
+      {/* This tool has been built by a team of researchers at the Zurich University of Applied Sciences, the University of Zurich and the University of St. Gallen.
+      Learn more about us here: <a href="https://fair-ai.ch" target="_blank">https://fair-ai.ch</a> */}
     </div>
   );
 }
