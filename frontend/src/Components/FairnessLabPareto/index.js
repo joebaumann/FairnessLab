915c3a86cdc197c0015a7fc90208dc2eb0fe2c71
hcorinna <12029382+hcorinna@users.noreply.github.com>
2022-03-10 23:37:42 +0100

    Subjects' utilities and fairness score calculation

--- frontend/src/Components/FairnessLabPareto/index.js
+++ frontend/src/Components/FairnessLabPareto/index.js
@@ -3,6 +3,7 @@ import React, {useState} from 'react';
 import Header from '../Header';
 import ScoreDistribution from '../ScoreDistribution';
 import ParetoPlot from '../ParetoPlot';
+import SubjectsUtility from '../SubjectsUtility';
 import scores from '../../data_static/compas/scores.json';
 import y from '../../data_static/compas/y.json';
 import decisionmaker_utility from '../../data_static/compas/static_pareto/decisionmaker_utility.json';
@@ -18,6 +19,7 @@ function FairnessLabPareto() {
       <Header title="Fairness Lab: Pareto"/>
       <ParetoPlot scores={[scores["scores_group1"], scores["scores_group2"]]} group1={group1} setGroup1={setGroup1} group2={group2} setGroup2={setGroup2} selectedPoints={selectedPoints} setSelectedPoints={setSelectedPoints} colors={colors} setColors={setColors} />
       <ScoreDistribution scores={[scores["scores_group1"], scores["scores_group2"]]} y={[y["y_group1"], y["y_group2"]]} selectedPoints={selectedPoints} labels={[group1, group2]} colors={colors}/>
+      <SubjectsUtility scores={[scores["scores_group1"], scores["scores_group2"]]} group1={group1} group2={group2} selectedPoints={selectedPoints} colors={colors} />
     </div>
   )
 }
---
+++ frontend/src/Components/SubjectsUtility/index.js
@@ -0,0 +1,53 @@
+import React, {useState} from 'react';
+import Plot from 'react-plotly.js';
+import './SubjectsUtility.css';
+import subjects_utility from '../../data_static/compas/static_pareto/subjects_utility.json';
+import fairness_score from '../../data_static/compas/static_pareto/fairness_score.json';
+
+const ParetoPlot = ({scores, labels, group1, group2, selectedPoints, colors}) => {
+    const selectedUtilities = [];
+    const colorsUtilities = []
+    const groupLabels = []
+    const selectedFairnessScore = []
+    const colorsFairnessScore = []
+    const labelsFairnessScore = []
+    selectedPoints.forEach(i => {
+        selectedUtilities.push(subjects_utility[i][0])
+        selectedUtilities.push(subjects_utility[i][1])
+        selectedFairnessScore.push(fairness_score[i])
+        colorsUtilities.push(colors[i])
+        colorsUtilities.push(colors[i])
+        colorsFairnessScore.push(colors[i])
+        groupLabels.push(group1 + ' ' + i.toString())
+        groupLabels.push(group2 + ' ' + i.toString())
+        labelsFairnessScore.push('Fairness score ' + i.toString())
+    })
+    
+    return (
+        <div className='SubjectsUtility'>
+            <h1>Subjects' utility for fairness score calculation</h1>
+            <Plot
+                data={[
+                {
+                    x: groupLabels,
+                    y: selectedUtilities,
+                    marker:{color: colorsUtilities},
+                    type: 'bar',
+                },
+                ]}
+            />
+            <Plot
+                data={[
+                {
+                    x: labelsFairnessScore,
+                    y: selectedFairnessScore,
+                    marker:{color: colorsFairnessScore},
+                    type: 'bar',
+                },
+                ]}
+            />
+        </div>
+      );
+}
+
+export default ParetoPlot;
