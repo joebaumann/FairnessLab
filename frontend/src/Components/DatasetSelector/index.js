b43f176e494ce9d235804b7c989f5fac270588f8
hcorinna <12029382+hcorinna@users.noreply.github.com>
2022-03-24 17:20:48 +0100

    Allow users to upload their own score file

--- frontend/src/Components/DatasetSelector/index.js
+++ frontend/src/Components/DatasetSelector/index.js
@@ -25,21 +25,76 @@ function DatasetSelector({setScores, setY}) {
     }
 
     const [datasetSelection, setDatasetSelection] = useState('Credit Lending');
+	const [fileName, setFileName] = useState(null);
+    const [fileScores, setFileScores] = useState(null)
+    const [fileError, setFileError] = useState(false);
+
+    function handleFile(e) {
+        const content = e.target.result;
+        console.log('file content',  content)
+        const text = (e.target.result)
+        const jsonScores = JSON.parse(text)
+        setFileScores(jsonScores)
+      }
+      
+
+    // https://medium.com/@miniceo/front-end-shorts-how-to-read-content-from-the-file-input-in-react-17f49b293909
+    const selectScoresFile = async (e) => {
+        if (e?.target?.files.length > 0) {
+            e.preventDefault()
+            let fileName = e.target.files[0]['name']
+            setFileName(fileName)
+            const reader = new FileReader()
+            reader.onload = handleFile
+            reader.readAsText(e.target.files[0])
+        }
+        else {
+            setFileError(false)
+            setFileName(null)
+        }
+    }
 
     useEffect(() => {
         setScores(datasets[datasetSelection]['scores'])
         setY(datasets[datasetSelection]['y'])
+        setFileError(true)
     }, [datasetSelection]);
+
+    useEffect(() => {
+        if (fileScores !== null
+            && fileScores.hasOwnProperty('scores_group1')
+            && fileScores['scores_group1'].length === datasets[datasetSelection]['y']['y_group1'].length
+            && fileScores.hasOwnProperty('scores_group2')
+            && fileScores['scores_group2'].length === datasets[datasetSelection]['y']['y_group2'].length) {
+            setScores(fileScores)
+            setFileError(false)
+        }
+        else {
+            setFileError(true)
+        }
+    }, [fileScores]);
 
     return(
         <div className="DatasetSelector">
         <h1>Dataset</h1>
-        <label htmlFor="datasetSelection">Choose a dataset:</label>
+        <label htmlFor="datasetSelection">Choose a dataset</label>
         <select name="datasetSelection" id="pattern" value={datasetSelection} onChange={(e) => setDatasetSelection(e.target.value)}>
             <option value="Credit Lending">Credit Lending</option>
             <option value="Algorithmic Hiring">Algorithmic Hiring</option>
             <option value="COMPAS">COMPAS</option>
         </select>
+        <br/>
+        <label htmlFor="scoreUpload">Upload your predictions:</label>
+        <input type="file" name="file" onChange={selectScoresFile} />
+        {fileName === null &&
+            <div>N.B.: No file has been uploaded, so the default scores for {datasetSelection} will be used.</div>
+        }
+        {fileName !== null && fileError &&
+            <div>N.B.: Selected "{fileName}", but the scores don't match the dataset, so the default scores for {datasetSelection} will be used.</div>
+        }
+        {fileName !== null && !fileError &&
+            <div>Selected "{fileName}"</div>
+        }
         </div>
     )
 }
