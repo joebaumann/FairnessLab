811e3148faa6e2f79b959b1c0b6b962279d77739
hcorinna <12029382+hcorinna@users.noreply.github.com>
2022-09-16 03:46:24 +0200

    Added ACS datasets from folktables

--- frontend/src/Components/DatasetSelector/index.js
+++ frontend/src/Components/DatasetSelector/index.js
@@ -2,6 +2,7 @@ import './DatasetSelector.css';
 import React, {useState, useEffect} from 'react';
 import compas_file from '../../data_static/compas/compas.json';
 import german_file from '../../data_static/credit_lending/german.json';
+import ACSEmploymentCA_file from '../../data_static/ACS/ACSEmployment_CA.json';
 
 function DatasetSelector({datasetSelection, setDatasetSelection, setScores, setY, justifier, datasetSelectionCounter, setDatasetSelectionCounter}) {
     const datasets = {
@@ -10,6 +11,9 @@ function DatasetSelector({datasetSelection, setDatasetSelection, setScores, setY
         },
         'German': {
             'file': german_file
+        },
+        'ACSEmploymentCA': {
+            'file': ACSEmploymentCA_file
         },
         'Own': {
             'file': []
@@ -129,12 +133,35 @@ function DatasetSelector({datasetSelection, setDatasetSelection, setScores, setY
     return(
         <div className="DatasetSelector">
         <h1>Dataset</h1>
-        <div>Choose a dataset that you want to audit. If you want to upload your own dataset, please make sure that it has a column named 'Y' (only 0 and 1 allowed), a column named 'scores' (values have to be between 0 and 1) and a column named 'sensitive-attribute' (only 0 and 1 allowed).</div>
+        <div>Choose a dataset that you want to audit. If you want to upload your own dataset, please make sure that it has (1) a column named 'Y' (only 0 and 1 allowed), (2) a column named 'sensitive-attribute' (only 0 and 1 allowed) and (3) a column named 'scores' (values have to be between 0 and 1) and/or a column named 'D' (only 0 and 1 allowed).</div>
         <br/>
         <span value={datasetSelection} onChange={(e) => setDatasetSelection(e.target.value)}>
             <input name="datasetSelection" type="radio" value="COMPAS" checked={datasetSelection === "COMPAS"} /> COMPAS
+            <div>The COMPAS dataset was collected by ProPublica for their article "Machine Bias." We <a href="https://github.com/propublica/compas-analysis" target="_blank">preprocessed</a> the dataset to make it usable for this demo. The predicted scores are the original (decimal) scores from COMPAS.</div>
+            <ul>
+                <li>Y=0: Was arrested within two years</li>
+                <li>Y=1: Was not arrested within two years</li>
+                <li>D=0: Predicted to be rearrested</li>
+                <li>D=1: Predicted not to be rearrested</li>
+            </ul>
             <br/>
             <input name="datasetSelection" type="radio" value="German" checked={datasetSelection === "German"} /> Credit lending (UCI German Credit)
+            <div>The German Credit dataset is available in the UCI repository. It is a small dataset of German credit loans from the 1970s. The scores have been predicted with a vanilla logistic regression.</div>
+            <ul>
+                <li>Y=0: Defaulted on the loan</li>
+                <li>Y=1: Repaid the loan</li>
+                <li>D=0: Predicted to default</li>
+                <li>D=1: Predicted to repay</li>
+            </ul>
+            <br/>
+            <input name="datasetSelection" type="radio" value="ACSEmploymentCA" checked={datasetSelection === "ACSEmploymentCA"} /> ACSEmployment (California)
+            <div>The ACSEmployment dataset is derived from US Census data and is available through the Folktables GitHub repository. It is a large dataset of US adults from California. The task is to predict whether an individual is employed. The scores have been predicted with a vanilla logistic regression.</div>
+            <ul>
+                <li>Y=0: Is not employed</li>
+                <li>Y=1: Is employed</li>
+                <li>D=0: Predicted to be unemployed</li>
+                <li>D=1: Predicted to be employed</li>
+            </ul>
             <br/>
             <input name="datasetSelection" type="radio" value="Own" checked={datasetSelection === "Own"} /> Choose your own dataset: 
         </span>
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -7,8 +7,11 @@ module.exports = global.config = {
         'German': {
             'group1': 'female',
             'group2': 'male'
-        }
-        ,
+        },
+        'ACSEmploymentCA': {
+            'group1': 'Black',
+            'group2': 'white'
+        },
         'Own': {
             'group1': 'group 1',
             'group2': 'group 2'
