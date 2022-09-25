ecbabe51b88450764f123b37c30b7a5e4801315a
hcorinna <12029382+hcorinna@users.noreply.github.com>
2022-09-25 16:05:45 +0200

    Updated FAQ a lil

--- frontend/src/Components/OtherPages/FAQ.js
+++ frontend/src/Components/OtherPages/FAQ.js
@@ -12,42 +12,48 @@ function FAQ() {
             <p>
             You can upload your dataset as either a CSV or a JSON file.
             You will need the following attributes:
+            </p>
             <ul>
                 <li><b>sensitive-attribute</b>: <i>binary (0 or 1)</i>, defines the salient groups that you want to compare. These could be groups based on gender, race, age, disability status, sexual orientation etc.</li>
                 <li><b>Y</b>: <i>binary (0 or 1)</i>, the target variable, what the model tries to predict</li>
                 <li><b>scores</b>: <i>continuous variable (between 0 and 1)</i>, what the model predicts as the probability of the individual having Y=1</li>
                 <li><b>D</b>: <i>binary (0 or 1)</i>, the decision of the model</li>
             </ul>
+            <p>
             Whether you upload a CSV or JSON file, you can always add more attributes to your file, but only the ones listed above will be considered.
             </p>
             <p>
             If you want to upload your dataset as a <b>CSV</b> file, make sure to use a comma as the separator. The structure should look as follows:
             </p>
             <table>
-                <tr>
-                    <th>sensitive-attribute</th>
-                    <th>Y</th>
-                    <th>scores</th>
-                    <th>D</th>
-                </tr>
-                <tr>
-                    <td>1</td>
-                    <td>1</td>
-                    <td>0.7</td>
-                    <td>1</td>
-                </tr>
-                <tr>
-                    <td>0</td>
-                    <td>1</td>
-                    <td>0.8</td>
-                    <td>1</td>
-                </tr>
-                <tr>
-                    <td>1</td>
-                    <td>0</td>
-                    <td>0.6</td>
-                    <td>1</td>
-                </tr>
+                <thead>
+                    <tr>
+                        <th>sensitive-attribute</th>
+                        <th>Y</th>
+                        <th>scores</th>
+                        <th>D</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>0.7</td>
+                        <td>1</td>
+                    </tr>
+                    <tr>
+                        <td>0</td>
+                        <td>1</td>
+                        <td>0.8</td>
+                        <td>1</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>0</td>
+                        <td>0.6</td>
+                        <td>1</td>
+                    </tr>
+                </tbody>
             </table>
             <p>
                 If you want to upload a <b>JSON</b> file, make sure it looks like the following with one object per individual:
@@ -63,14 +69,15 @@ function FAQ() {
 
             <h3>I don't have the scores that the model outputs ('scores'), only the final decisions ('D'). How does this affect the audit?</h3>
             <p>
-                No points from postprocessing in Pareto plot, only some basic points (like perfect predictor) to compare decisions against.
+                The Pareto plot only shows the utility of the decision-maker and the fairness score for the decisions in the dataset (the column 'D').
             </p>
 
             <h3>What does the Pareto plot show?</h3>
             <p>
                 If 'scores' are available:
+            </p>
+            <p>
                 If 'D' is available:
-                Always:
             </p>
 
             <h3>What makes the FairnessLab different from tools like Aequitas, AIF360 or FairLearn?</h3>
