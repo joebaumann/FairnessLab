157933c45633980b6c64ccc7f11123e4dee9016a
hcorinna <12029382+hcorinna@users.noreply.github.com>
2022-10-25 17:08:23 +0200

    State management

--- frontend/src/Components/Audit/index.js
+++ frontend/src/Components/Audit/index.js
@@ -14,7 +14,6 @@ import { getFairnessScoreDescription } from '../../store/fairnessScore';
 function Audit(props) {
   // the datasetSelectionCounter is incremented every time the datasetSelection or the justifier changes
   // this makes sure that the decision subject utilities are recalculated after the justifier has been applied to the dataset
-  const [datasetSelectionCounter, setDatasetSelectionCounter] = useState(0);
   const [colors, setColors] = useState(Array(11 * 11).fill('#4e87ad'));
   const fairnessScoreDescription = useSelector(getFairnessScoreDescription);
 
@@ -22,7 +21,7 @@ function Audit(props) {
     <div className="Audit">
       <Header title="Audit"/>
       <div className="Content">
-        <DatasetSelector datasetSelectionCounter={datasetSelectionCounter} setDatasetSelectionCounter={setDatasetSelectionCounter} />
+        <DatasetSelector/>
         <Terminology/>
         <Configuration/>
         <h1>Audit</h1>
@@ -30,7 +29,7 @@ function Audit(props) {
         In the audit, we will use the fairness metric that you defined with your inputs above. Specifically, we will look at the following fairness metric:
         <div><i>{fairnessScoreDescription}</i></div>
         <SubjectsUtility colors={colors} />
-        <ParetoPlot colors={colors} setColors={setColors} datasetSelectionCounter={datasetSelectionCounter} />
+        <ParetoPlot colors={colors} setColors={setColors} />
         <SelectedPointsTable />
         <ScoreDistribution colors={colors} />
       </div>
