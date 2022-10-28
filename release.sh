cf8d23f0e52fd0eadfb5f7191063590b0abd5903
Joachim Baumann <joachimbaumann1@gmail.com>
2022-10-28 11:24:45 +0200

    release script now handles private&public gh-pages

--- release.sh
+++ release.sh
@@ -3,7 +3,9 @@
 # created released branch with command: git checkout -b releases bd384c69bb86aa6965da8dc2fb6a4e72e835774d
 # created remote for public repo with: git remote add public-releases https://github.com/joebaumann/FairnessLab.git
 
-echo "Have you already updated the changelog file and set the package.json homepage to 'https://joebaumann.github.io/FairnessLab'? (yes/no)"
+echo "Is your answer 'yes' for both of these questions? (yes/no)"
+echo "  Have you already updated the changelog file?"
+echo "  Are you in the FairnessLab-private directory?"
 
 read input
 if [ "$input" == "yes" ]
@@ -15,6 +17,16 @@ then
     echo "which branch do you want to release?"
     read branch_to_release
     git merge $branch_to_release --no-ff --no-commit
+    echo "automatically change gh-pages homepage for public repo (only works on Mac/Linux)? (yes/no)"
+    read input_homepage
+    if [ "$input_homepage" == "yes" ]
+    then
+        sed -i 's/"homepage": "https:\/\/joebaumann.github.io\/FairnessLab-private",/"homepage": "https:\/\/joebaumann.github.io\/FairnessLab",/g' frontend/package.json
+    else
+        echo 'Change the homepage from "https://joebaumann.github.io/FairnessLab-private" to "https://joebaumann.github.io/FairnessLab" in the package.json file and save the file?'
+        echo 'Press enter once you are done.'
+        read continue_with_script
+    fi
     echo "check if there are any merge conflicts and type 'continue' to add all files, push, and commit:"
     read input_push
     if [ "$input_push" == "continue" ]
